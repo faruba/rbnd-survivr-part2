904fddafa87a1436ee82957c1e7532461157fc7e
faruba <farubafaruba@126.com>
2016-05-26 04:25:57 +0800

    test suggestion

--- lib/jury.rb
+++ lib/jury.rb
@@ -9,11 +9,8 @@ class Jury
   end
 
   def cast_votes(name_list)
-    # some thing like this does not work : ret = Hash name_list.map{ |name| [name,0]}
-    # or ret = [name_list,Array.new(name_list.length,0)].to_h
-    # I have to do like this
-    ret = Hash.new
-    name_list.each {|name| ret[name] = 0}
+    #awesome !
+    ret = Hash[name_list.collect { |name| [name, 0] }]
 
     @members.each{ |member|
       vote_to = name_list.sample
@@ -28,13 +25,7 @@ class Jury
     }
   end
   def announce_winner(result)
-    max_votes = 0
-    result.each{| member, votes|
-      if(votes > max_votes)
-        result = member
-        max_votes = votes
-      end
-    }
-    return  result
+    #awesome !
+    result.max_by{ | member, votes| votes}[0]
   end
 end
